091e57bbc160e2066ab75db72ee78fb3a75746f5
yara mohamed <75863913+yara857@users.noreply.github.com>
2023-07-19 19:51:51 +0300

    Delete bank directory

--- bank/myapp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import *
-class TransferForm(forms.ModelForm):
-    class Meta:
-        model = Transaction
-        fields = ['sender', 'receiver', 'amount']
